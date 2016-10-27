756418d1f4a013c7a72c071c23de212c86f1ef95
Matthias Pfeil <matthias.pfeil@wwu.de>
2016-10-27 22:45:17 +0200

    add basic test

---
+++ test/spec/angular-opensensemap.spec.js
@@ -0,0 +1,13 @@
+'use strict';
+/* global getJSONFixture */
+describe('angular-opensensemap', function () {
+
+  describe('openSenseMapProvider', function () {
+
+    var openSenseMapProvider = {};
+
+    it('should be defined', function () {
+      expect(openSenseMapProvider).toBeDefined();
+    });
+  });
+});
