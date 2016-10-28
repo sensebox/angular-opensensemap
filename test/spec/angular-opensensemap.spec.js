51e88ee7d3a8eef0d0d230c2ce0ad0bdd917aeec
Matthias Pfeil <matthias.pfeil@wwu.de>
2016-10-28 09:45:34 +0200

    test specs for provider and injector

--- test/spec/angular-opensensemap.spec.js
+++ test/spec/angular-opensensemap.spec.js
@@ -2,12 +2,62 @@
 
 describe('angular-opensensemap', function () {
 
-  describe('openSenseMapProvider', function () {
+  describe('OpenSenseMapProvider', function () {
 
-    var openSenseMapProvider = {};
+    var OpenSenseMapProvider = {};
+
+    beforeEach(function () {
+      module('openSenseMap', function (_OpenSenseMapProvider_) {
+        OpenSenseMapProvider = _OpenSenseMapProvider_;
+      });
+      inject(function () {});
+    });
 
     it('should be defined', function () {
-      expect(openSenseMapProvider).toBeDefined();
+      expect(OpenSenseMapProvider).toBeDefined();
     });
+
+    it('should have a method setBoxId()', function () {
+      expect(OpenSenseMapProvider.setBoxId).toBeDefined();
+    });
+
+    it('should have a method getBoxId()', function () {
+      expect(OpenSenseMapProvider.getBoxId).toBeDefined();
+    });
+
+    it('should have a method setApiKey()', function () {
+      expect(OpenSenseMapProvider.setApiKey).toBeDefined();
+    });
+  });
+
+  describe('OpenSenseMap', function () {
+
+    beforeEach(module('openSenseMap'));
+
+    var OpenSenseMap;
+
+    beforeEach(inject(function (_OpenSenseMap_) {
+      OpenSenseMap = _OpenSenseMap_;
+    }));
+
+    it('should be defined', function () {
+      expect(OpenSenseMap).toBeDefined();
+    });
+
+    it('should be an object', function () {
+      expect(typeof OpenSenseMap).toBe('object');
+    });
+
+    it('should have a method api()', function () {
+      expect(OpenSenseMap.api).toBeDefined();
+    });
+
+    it('should have a method getBoxes()', function () {
+      expect(OpenSenseMap.getBoxes).toBeDefined();
+    });
+
+    it('should have a method getBox()', function () {
+      expect(OpenSenseMap.getBox).toBeDefined();
+    })
   });
 });
