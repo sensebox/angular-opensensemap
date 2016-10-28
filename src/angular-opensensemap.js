e89365619f49e478b36a8de1ba6e8714a2dbcebb
Matthias Pfeil <matthias.pfeil@wwu.de>
2016-10-28 09:46:30 +0200

    add osem module

---
+++ src/angular-opensensemap.js
@@ -0,0 +1,71 @@
+(function (window, angular, undefined) {
+  'use strict';
+
+  angular
+    .module('openSenseMap', [])
+    .provider('OpenSenseMap', function () {
+
+      // Module global settings
+      var settings = {};
+      settings.boxId = null;
+      settings.apiKey = null;
+
+      this.setBoxId = function (boxId) {
+        settings.boxId = boxId;
+        return settings.boxId;
+      };
+
+      this.getBoxId = function () {
+        return settings.boxId;
+      };
+
+      this.setApiKey = function (apiKey) {
+        settings.apiKey = apiKey;
+        return settings.apiKey;
+      };
+
+      /**
+       * API Base URL
+       */
+      settings.apiBaseUrl = 'https://api.opensensemap.org';
+
+      this.$get = ['$q', '$http', '$window', function ($q, $http, $window) {
+        function NgOpenSenseMap () {
+          this.boxId = settings.boxId;
+          this.apiKey = settings.apiKey;
+        }
+
+        NgOpenSenseMap.prototype = {
+          api: function (endpoint, method, params, data, headers) {
+            var deferred = $q.defer();
+
+            $http({
+              url: this.apiBaseUrl + endpoint,
+              method: method ? method : 'GET',
+              params: params,
+              data: data,
+              headers: headers,
+              withCredentials: false
+            })
+            .success(function (data) {
+              deferred.resolve(data);
+            })
+            .error(function (data) {
+              deferred.reject(data);
+            });
+            return deferred.promise;
+          },
+          getBoxes: function () {
+            return this.api('/boxes');
+          },
+          getBox: function (boxId) {
+            return this.api('/boxes/' + boxId);
+          }
+        };
+
+        return new NgOpenSenseMap();
+      }];
+
+    });
+
+}(window, angular));
